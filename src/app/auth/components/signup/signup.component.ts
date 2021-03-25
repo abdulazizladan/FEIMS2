098183fb05f5e7e1e20e3ec54728926333d27b62
abdulazizladan <abdulazizladan@gmail.com>
2021-03-25 12:49:36 +0100

    task: Add signup component in auth module

--- src/app/auth/components/signup/signup.component.ts
+++ src/app/auth/components/signup/signup.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatProgressBar } from '@angular/material/progress-bar';
+import { Validators, FormGroup, FormControl } from '@angular/forms';
+import { CustomValidators } from 'ngx-custom-validators';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  @ViewChild(MatProgressBar) progressBar: MatProgressBar;
+  @ViewChild(MatButton) submitButton: MatButton;
 
-  constructor() { }
+  signupForm: FormGroup
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    const password = new FormControl('', Validators.required);
+    const confirmPassword = new FormControl('', CustomValidators.equalTo(password));
+
+    this.signupForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: password,
+      confirmPassword: confirmPassword,
+      agreed: new FormControl('', (control: FormControl) => {
+        const agreed = control.value;
+        if(!agreed) {
+          return { agreed: true }
+        }
+        return null;
+      })
+    })
+  }
+
+  signup() {
+    const signupData = this.signupForm.value;
+    console.log(signupData);
+
+    this.submitButton.disabled = true;
+    this.progressBar.mode = 'indeterminate';
   }
 
 }
