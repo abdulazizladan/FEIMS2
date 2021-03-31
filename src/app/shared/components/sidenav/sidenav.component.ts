64fa3a6445e88782a2c1a7d6f7938e5f86d69f15
abdulazizladan <abdulazizladan@gmail.com>
2021-03-31 12:07:41 +0100

    task: Fix department selection feature

--- src/app/shared/components/sidenav/sidenav.component.ts
+++ src/app/shared/components/sidenav/sidenav.component.ts
@@ -1,14 +1,62 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+export interface faculty{
+  'name': string,
+  'site': string,
+  'code': string
+}
+
+export interface department{
+  'name': string,
+  'faculty': string,
+  'code': string
+}
+
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.template.html'
 })
 export class SidenavComponent {
 
-  site: string;
-  faculty: string;
-  department: string;
+  selectedSite: string;
+  selectedFaculty: string;
+  selectedDepartment: string;
+
+  sites: string[] = [
+    'samaru',
+    'kongo'
+  ]
+
+  faculties: faculty[] = [
+    {
+      name: 'law',
+      site: 'kongo',
+      code: 'law'
+    },
+    {
+      name: 'administration',
+      site: 'kongo',
+      code: 'adm'
+    },
+    {
+      name: 'sciences',
+      site: 'samaru',
+      code: 'sci'
+    },
+    {
+      name: 'engineering',
+      site: 'samaru',
+      code: 'eng'
+    }
+  ] 
+
+  departments: department[] = [
+    {
+      name: 'civil law',
+      faculty: 'law',
+      code: 'cvl'
+    }
+  ]
   
   @Input('items') public menuItems: any[] = [];
   @Input('hasIconMenu') public hasIconTypeMenuItem: boolean;
@@ -16,6 +64,14 @@ export class SidenavComponent {
 
   constructor() {}
   ngOnInit() {}
+
+  getFaculties(){
+    return this.faculties.filter((fac) => fac.site == this.selectedSite);
+  }
+
+  getDepartments(){
+    return this.departments.filter((dep) => dep.faculty == this.selectedFaculty);
+  }
 
   // Only for demo purpose
   addMenuItem() {
