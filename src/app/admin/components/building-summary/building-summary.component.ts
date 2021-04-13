09aafc95b9b948da2d1fb53fdaff3a52011da0ae
Abdulaziz Ladan <abdulazizladan@gmail.com>
2021-04-13 15:48:12 +0100

    task: Position mat-dialog close button to top right corner

--- src/app/admin/components/building-summary/building-summary.component.ts
+++ src/app/admin/components/building-summary/building-summary.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { egretAnimations } from '../../../shared/animations/egret-animations';
+import { ThemeService } from '../../../shared/services/theme.service';
+import tinyColor from 'tinycolor2';
 
 @Component({
   selector: 'app-building-summary',
   templateUrl: './building-summary.component.html',
-  styleUrls: ['./building-summary.component.scss']
+  styleUrls: ['./building-summary.component.scss'],
+  animations: egretAnimations
 })
 export class BuildingSummaryComponent implements OnInit {
 
