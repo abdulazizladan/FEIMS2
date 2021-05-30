defd1b9f1816d3deb73a76fc24ff606dbe519b3f
Abdulaziz Ladan <abdulazizladan@gmail.com>
2021-05-30 22:05:36 +0100

    fix: Fix add_site model

--- src/app/admin/models/site.model.ts
+++ src/app/admin/models/site.model.ts
@@ -1,5 +1,12 @@
 export interface Site{
     name : string,
     code : string,
-    
+    state: string,
+    lga: string,
+    street_address: string,
+    site_measurement: number,
+    site_level: number,
+    site_longitude: number,
+    site_latitude: number,
+    comment: string
 }
