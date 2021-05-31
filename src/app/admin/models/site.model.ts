9b204b7d290a5a3addbe4e0435a9cf91cc7efb5e
Abdulaziz Ladan <abdulazizladan@gmail.com>
2021-05-31 10:54:30 +0100

    task: Populate fence portion of site model

--- src/app/admin/models/site.model.ts
+++ src/app/admin/models/site.model.ts
@@ -1,4 +1,6 @@
+/* Site form data */
 export interface Site{
+    /* Site general data */
     name : string,
     code : string,
     state: string,
@@ -8,6 +10,28 @@ export interface Site{
     site_level: number,
     site_longitude: number,
     site_latitude: number,
-    comment: string,
-    gate_description: string
+    site_comment: string,
+    /* Gate */
+    gate_description: string,
+    gate_size: number,
+    gate_thickness: number,
+    gate_quantity: number,
+    gate_condition: string,
+    gate_damage: number,
+    gate_cost_of_repair: number,
+    /* Fence */
+    fence_descriptio: string,
+    fence_height: number,
+    perimeter: number,
+    fence_quantity: number,
+    fence_concrete_work: number,
+    fence_block_work: number,
+    fence_form_work: number,
+    fence_reinforcement_work: number,
+    fence_rendering: number,
+    fence_barb_wire: number,
+    fence_condition: string,
+    fence_damage: number,
+    fence_cost_of_repair: number,
+    /* */
 }
