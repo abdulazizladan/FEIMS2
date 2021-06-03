b68094050d3e39a2143cb176d2308cdf632d1819
Abdulaziz Ladan <abdulazizladan@gmail.com>
2021-06-03 11:20:51 +0100

    task: Init add-site form groups

--- src/app/admin/components/add-site/add-site.component.ts
+++ src/app/admin/components/add-site/add-site.component.ts
@@ -11,9 +11,69 @@ export class AddSiteComponent implements OnInit {
 
   expansionPanelStep: number = 0;
 
+  public firstFormGroup: FormGroup;//Site Survey
+  public secondFormGroup: FormGroup;//Gate
+  public thirdFormGroup: FormGroup;//Fence
+  public fourthFormGroup: FormGroup;//Roads
+  public fifthFormGroup: FormGroup;//Paths & Pavements
+  public sixthFormGroup: FormGroup;//Driveway
+  public seventhFormGroup: FormGroup;//Drainage
+  public eighthFormGroup: FormGroup;//Electricity
+  public ninthFormGroup: FormGroup;//Water
+  public tenthFormGroup: FormGroup;//Others
+
   constructor( private siteService: SiteService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    //
+    this.firstFormGroup = this.fb.group({
+
+    })
+    
+    //
+    this.secondFormGroup = this.fb.group({
+
+    })
+
+    //
+    this.thirdFormGroup = this.fb.group({
+
+    })
+
+    //
+    this.fourthFormGroup = this.fb.group({
+
+    })
+
+    //
+    this.fifthFormGroup = this.fb.group({
+
+    })
+
+    //
+    this.sixthFormGroup = this.fb.group({
+
+    })
+
+    //
+    this.seventhFormGroup = this.fb.group({
+
+    })
+
+    //
+    this.eighthFormGroup = this.fb.group({
+
+    })
+
+    //
+    this.ninthFormGroup = this.fb.group({
+
+    })
+
+    //
+    this.tenthFormGroup = this.fb.group({
+
+    })
   }
 
   setPanelStep( index: number) {
