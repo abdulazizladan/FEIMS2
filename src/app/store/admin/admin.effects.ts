bd139d997e68809ba70a7ed46138240338e5933a
Abdulaziz Ladan <abdulazizladan@gmail.com>
2021-07-02 13:32:42 +0100

    feat: Create building effect

--- src/app/store/admin/admin.effects.ts
+++ src/app/store/admin/admin.effects.ts
@@ -1,9 +1,10 @@
 import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { SiteService } from "app/admin/services/site.service";
+import { BuildingService } from "app/admin/services/building.service";
 import { of } from "rxjs";
 import { catchError, exhaustMap, map } from "rxjs/operators";
-import { addSite, addSiteFail, addSiteSuccess } from "./admin.actions";
+import { addSite, addSiteFail, addSiteSuccess, addBuilding, addBuildingSuccess, addBuildingFail } from "./admin.actions";
 
 @Injectable()
 export class AdminEffect {
@@ -16,5 +17,13 @@ export class AdminEffect {
     ))
   ));
 
-  constructor(private actions$: Actions, private siteService: SiteService) {}
+  addBuilding$ = createEffect(() => this.actions$.pipe(
+    ofType(addBuilding),
+    exhaustMap(({building}) => this.buildingService.addBuilding(building).pipe(
+      map(building => addBuildingSuccess({building})),
+      catchError(error => of(addBuildingFail({reason: error})))
+    ))
+  ));
+
+  constructor(private actions$: Actions, private siteService: SiteService, private buildingService: BuildingService) {}
 }
