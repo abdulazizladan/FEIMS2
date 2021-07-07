860c7426362652656c834fe35cebda952788e477
Abdulaziz Ladan <abdulazizladan@gmail.com>
2021-07-07 13:38:39 +0100

    fix: Fix paramterized route param

--- src/app/admin/components/building-summary/building-summary.component.ts
+++ src/app/admin/components/building-summary/building-summary.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { BuildingService } from 'app/admin/services/building.service';
+import { building } from 'app/admin/models/building.model';
 import { egretAnimations } from '../../../shared/animations/egret-animations';
+import { ActivatedRoute } from '@angular/router';
 import { ThemeService } from '../../../shared/services/theme.service';
 import * as tinyColor from 'tinycolor2';
 
@@ -11,9 +14,24 @@ import * as tinyColor from 'tinycolor2';
 })
 export class BuildingSummaryComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+  public building: building;
+  constructor( private buildingService: BuildingService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.id = parseInt(this.route.snapshot.paramMap.get('id'));
+    this.getBuildings(this.id);
+  }
+
+  getBuildings( id: number) {
+    this.buildingService.getSingleBuilding(id).subscribe(
+      result => {
+
+      },
+      error => {
+         
+      }
+    )
   }
 
 }
