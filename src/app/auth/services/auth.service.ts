0771b00a169c57965a855a9377ddb9490e8cc7ac
Abdulaziz Ladan <abdulazizladan@gmail.com>
2021-07-12 15:55:49 +0100

    task: Document auth service

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -24,39 +24,69 @@ export class AuthService {
   private readonly loginUrl: string = `${this.baseUrl}/${ApiPaths.signin}`;
   private readonly registerUrl: string = `${this.baseUrl}/${ApiPaths.signup}`;
   
-
+  /**
+   * 
+   * @param _http 
+   * @param appSocket 
+   */
   constructor( private _http: HttpClient, private appSocket: AppSocket) {
 
    }
 
+   /**
+    * @param credentials
+    */
   login(credentials: LoginData){
     return this._http.post<any>( this.loginUrl, credentials, httpOptions)
   }
 
+  /**
+   * @param value
+   */
   register(value: {email: string, password: string}){
     return this._http.post<any>( this.registerUrl, value)
   }
 
+  /**
+   * Reset password
+   * @param value
+   */
   resetPassword(value: string): boolean {
     return true;
   }
 
+  /**
+   * is logged in?
+   */
   isLoggedIn(): boolean{
     return !!localStorage.getItem('token');
   }
 
+  /**
+   * get JWT token
+   */
   getToken(){
     return localStorage.getItem('token');
   }
 
+  /**
+   * add user to cache
+   */
   addUserToCache(){
 
   }
 
+  /**
+   * get user from cache
+   */
   getUserFromCache(){
 
   }
 
+  /**
+   * Is user authenticated?
+   * @returns 
+   */
   isAuthenticated(): boolean{
     //if(local storage credentials are validated){
     //  return true
